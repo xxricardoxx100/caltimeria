041b6ee61a6be30a436361ce3aa26e361e6708ef
Ricardo <xxricardoxx7779@gmail.com>
2026-06-30 15:28:46 -0500

    Agrega --disable-gpu a todos los drivers para estabilidad en contenedor headless

--- soat.py
+++ soat.py
@@ -42,6 +42,7 @@ def crear_driver(headless: bool = True):
         options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1366,900")
     options.add_argument("--lang=es-PE")
 
